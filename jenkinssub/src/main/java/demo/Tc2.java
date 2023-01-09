fc45da7d29ca3acb479a0eedf806ab8e73d4eb50
ARJUNRAJ <ARJUNRAJ@ARJUNRAJLAPTOP>
2023-01-09 16:29:02 +0530

    FIRST

---
+++ jenkinssub/src/main/java/demo/Tc2.java
@@ -0,0 +1,13 @@
+package demo;
+
+import org.testng.annotations.Test;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Tc2 {
+	@Test
+	public void m2()
+	{
+		Reporter.log("Tc2 Running");
+		}
+}
